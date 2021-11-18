8b35ec4739d9bfb73a123b22d6c193f54eb2f57c
Wizard-lab-creator <80611597+Wizard-lab-creator@users.noreply.github.com>
2021-11-18 15:28:12 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+<ol>
+    <div class="test_image_and_result col-xs-12">
+        <li class="col-sm-4 col-xs-12">
+            <b>Test Image</b>
+            <img src="Vaseline.jpeg" class="img-responsive"></img>
+        </li>
+
+        <h5 class="col-sm-4 col-xs-12">
+            Output on <b>GOOGLE LENS</b> -
+            <span> </span>
+        </h5>
+
+        <h5 class="col-sm-4 col-xs-12">
+            Output on <b>MobileNet Model</b> -
+            <span> </span>
+        </h5>
+
+        <div class="mini_result" class="col-xs-12">
+        <h4>Result - </h4>
+        <hr>
+        <h4> </h4>
+        </div>
+        
+    </div>
+</ol>
